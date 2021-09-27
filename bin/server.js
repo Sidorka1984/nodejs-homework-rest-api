847f844c7b84dd897598b6888fcc1cc958dc930b
Olga Ulyanchyk <sidorka.olka@gmail.com>
2021-09-27 14:37:24 +0300

    add .env

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,18 @@
 const app = require('../app')
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+require("dotenv").config();
+
+const {DB_HOST} = process.env
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
+mongoose.connect(DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(()=> app.listen(PORT))
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
 })
