12959d4c89b8ee24d66bed67b56c473ce9d70377
Olga Ulyanchyk <sidorka.olka@gmail.com>
2021-10-11 22:35:24 +0300

    started

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,11 +2,15 @@ const signup = require("./signup");
 const login = require("./login");
 const logout = require("./logout");
 const current = require("./current");
+const updateSubscr = require("./updateSubscr");
+const avatars = require("./avatars");
 
 module.exports = {
     signup,
     login,
     logout,
-    current
+    current,
+    updateSubscr,
+    avatars
 }
  
